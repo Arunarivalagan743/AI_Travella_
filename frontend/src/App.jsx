6d626c0ecd2ea7b22f2aa8d3a6af1a5d72e396cf
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-06-08 19:12:00 +0530

    Installation of react and tailwind and shdcn and router dom set

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+    <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click me
+    </Button>
+  </h1>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from 'react-router-dom'
+import CreateTrip from './trip-maker/index.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path :'/',
+    element :<App />
+  },{
+    path: '/create-trip',
+    element :<CreateTrip />
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
+
